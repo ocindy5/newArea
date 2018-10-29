c2aa037a7d8244052e58cba3c8b4b3873d90bf55
Cindy Olifertchouk <cindy.olifertchouk@hotmail.fr>
2018-10-29 19:01:15 +0100

    price tables effect added

--- js/app.js
+++ js/app.js
@@ -177,3 +177,17 @@ $('#horror').click(function() {
     });
 
     }});
+
+
+// Tarifs effects
+
+$('.price-table').hover(function() {
+
+  $(this).css("transform", "scale(1.2");
+
+  }, function() {
+
+  $(this).css("transform", "scale(1)");
+
+
+})
